ca15d88f891aa3780ff2fc76010ee704364280d9
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-08-14 19:33:48 +0300

    Update CORS

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://anyware-exams.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
